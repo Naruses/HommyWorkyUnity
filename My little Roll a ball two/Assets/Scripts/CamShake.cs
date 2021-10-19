ace3645624806f40e2279fdbc9627fb92c2e0677
Naruses <telornvensen@gmail.com>
2021-10-19 19:05:56 +0300

    lessonthr
    thirdLesson

---
+++ My little Roll a ball two/Assets/Scripts/CamShake.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShake : MonoBehaviour
+{
+    public Animator camShake;
+    public void Shake()
+    {
+        camShake.SetTrigger("shake");
+    }
+}
