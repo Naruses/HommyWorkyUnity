bcbcfd08029ee59147480e5498197e5a81756c55
Naruses <telornvensen@gmail.com>
2021-11-09 19:08:57 +0300

    ready
    +-

---
+++ My little Roll a ball two/Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    public Transform PlayerTransf;
+
+    void LateUpdate()
+    {
+        UpdateCamPosition();
+    }
+    void UpdateCamPosition()
+    {
+        Vector3 newPos = PlayerTransf.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+
+        transform.rotation = Quaternion.Euler(90, PlayerTransf.eulerAngles.y, 0);
+    }
+}
